ec3ac12e4b5f77e766c6e51215386cca7646a9fe
Michael Necio <michael@necio.ca>
2016-08-11 13:22:21 -0400

    Added support for using previous cipher, and changed output to use Windows style line endings

--- bbq_cipher.cpp
+++ bbq_cipher.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cctype>
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <random>
@@ -9,24 +10,54 @@
 
 using namespace std;
 
+vector<char> getCipher();
+vector<char> getCipher(ifstream& cipher_file);
+
 int main(int argc, char* argv[]) {
-  vector<char> cipher('z' - 'a' + 1);
-  iota(cipher.begin(), cipher.end(), 'a');
-
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-  mt19937_64 rand_engine(seed);
-  shuffle(cipher.begin(), cipher.end(), rand_engine);
-
-  for (unsigned i = 0; i < cipher.size(); ++i) {
-    cout << '\n' << char('a' + i) << "  ->  " << cipher[i] << endl;
+  vector<char> cipher;
+  if(argc == 2) {
+    ifstream cipher_file(argv[1]);
+    if(cipher_file) {
+      cipher = getCipher(cipher_file);
+    } else {
+      cout << "Failed to open cipher file." << endl;
+      return -1;
+    }
+  } else {
+    cipher = getCipher();
+    /* We could have just printed out the cipher in getCipher()
+     * but that would be a naughty side effect */
+    for (unsigned i = 0; i < cipher.size(); ++i) {
+      cout << "\r\n" << char('a' + i) << "  ->  " << cipher[i] << endl;
+    }
+    cout << "\r\n\r\n";
   }
-
+  
   for (string message; getline(cin, message); /* empty */) {
     for (const auto& c : message) {
       cout << (isalpha(c) ? cipher[tolower(c) - 'a'] : c);
     }
-    cout << "\n\n\n\n\n";
+    cout << "\r\n\r\n\r\n\r\n\r\n";
   }
 
   return 0;
+}
+
+vector<char> getCipher() {
+  vector<char> cipher('z' - 'a' + 1);
+  iota(cipher.begin(), cipher.end(), 'a');
+
+  unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+  mt19937_64 rand_engine(seed);
+  shuffle(cipher.begin(), cipher.end(), rand_engine);
+
+  return cipher;
+}
+
+vector<char> getCipher(ifstream& cipher_file) {
+  vector<char> cipher('z' - 'a' + 1);
+  for(string pair; getline(cipher_file, pair); /* empty */) {
+    cipher[pair.front() - 'a'] = pair.back();
+  }
+  return cipher;
 }
